1da4493b7a6a7a17d1e680bf10242428db97769e
Alex <vasilax@gmail.com>
2020-09-19 02:00:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Configure the AWS Provider
  provider "aws" {
     access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
+    secret_key = "${var.aws_secret_key1}"
     region     = "${var.aws_region}"
     }
 
